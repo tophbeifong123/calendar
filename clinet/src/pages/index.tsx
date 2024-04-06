36ffae6f96959c053c64064a2d36768863fe9075
Chotmanat Chanakul <6610110074@email.psu.ac.th>
2024-04-06 22:34:47 +0700

    fullcarlendar

--- clinet/src/pages/index.tsx
+++ clinet/src/pages/index.tsx
@@ -1,11 +1,38 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import React from "react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import FullCalendar from "@fullcalendar/react";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export default function Home() {
-  return (
-    <main>
-    </main>
-  );
+function Calendar() {
+    return (
+        <div className="h-screen px-[15vh] pt-[7vh]">
+            <Link href="#_" className="relative inline-block text-lg group">
+                <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+                    <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+                    <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+                    <span className="relative">Button Sunny</span>
+                </span>
+                <span
+                    className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+                    data-rounded="rounded-lg"
+                ></span>
+            </Link>
+            <div className="">
+                <FullCalendar
+                    plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                    initialView={"dayGridMonth"}
+                    headerToolbar={{
+                        start: "today prev,next", // will normally be on the left. if RTL, will be on the right
+                        center: "title",
+                        end: "dayGridMonth,timeGridWeek,timeGridDay", // will normally be on the right. if RTL, will be on the left
+                    }}
+                    height={"70vh"}
+                />
+            </div>
+        </div>
+    );
 }
+
+export default Calendar;
