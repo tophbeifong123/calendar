8d0d8e31bba64993906e57f39c914b68695bbfe3
ธรรมีนา เพ็งชัย <thrrmunaphengchay@gmail.com>
2024-04-07 21:37:22 +0700

    'google_calendar'

--- clinet/src/pages/index.tsx
+++ clinet/src/pages/index.tsx
@@ -6,33 +6,42 @@ import FullCalendar from "@fullcalendar/react";
 import Link from "next/link";
 
 function Calendar() {
-    return (
-        <div className="h-screen px-[15vh] pt-[7vh]">
-            <Link href="#_" className="relative inline-block text-lg group">
-                <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
-                    <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
-                    <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
-                    <span className="relative">Button Sunny</span>
-                </span>
-                <span
-                    className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
-                    data-rounded="rounded-lg"
-                ></span>
-            </Link>
-            <div className="">
-                <FullCalendar
-                    plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-                    initialView={"dayGridMonth"}
-                    headerToolbar={{
-                        start: "today prev,next", // will normally be on the left. if RTL, will be on the right
-                        center: "title",
-                        end: "dayGridMonth,timeGridWeek,timeGridDay", // will normally be on the right. if RTL, will be on the left
-                    }}
-                    height={"70vh"}
-                />
-            </div>
-        </div>
-    );
+  const events = [
+    {
+      title: "กินข้าวกับครอบครัว",
+      start: new Date().setHours(18, 30),
+      end: new Date().setHours(20, 30),
+      allDay: false,
+    },
+    {
+      title: "ออกกำลังกาย",
+      start: new Date("2024-04-20"),
+      end: new Date("2024-04-20"),
+      allDay: true,
+    },
+  ];
+  return (
+    <div className="h-screen px-[15vh] pt-[7vh]">
+      <div className="">
+        <FullCalendar
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          initialView={"dayGridMonth"}
+          headerToolbar={{
+            start: "today prev,next",
+            center: "title",
+            end: "dayGridMonth,timeGridWeek,timeGridDay",
+          }}
+          height={"70vh"}
+          events={events}
+        />
+        <Link href="#_">
+          <button className="mt-10 text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+            Google & calendar
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Calendar;
