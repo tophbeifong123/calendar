e67f461101045e679d9cbe632b9413c8aa290993
Phodcharaphon <patt502090@hotmail.com>
2024-04-28 19:46:16 +0700

    change color on navbar a bit

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -5,7 +5,7 @@ import { Footer } from "flowbite-react";
 
 export function FooterComponent() {
   return (
-    <Footer container className="bg-[#000000] px-10">
+    <Footer container className="bg-[#222831] px-10">
       <Footer.Copyright href="/home" by="PSU Calendar™" year={2024} />
       <Footer.LinkGroup>
         <Footer.Link href="/home">Calendar</Footer.Link>
