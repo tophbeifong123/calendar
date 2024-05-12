6eb805fe8bae93c4b9b897551d3a8af2724d1cde
Chotmanat Chanakul <6610110074@email.psu.ac.th>
2024-05-12 19:21:05 +0700

    decorate

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -5,11 +5,11 @@ import { Footer } from "flowbite-react";
 
 export function FooterComponent() {
   return (
-    <Footer container className="bg-[#222831] px-10">
-      <Footer.Copyright href="/home" by="PSU Calendar™" year={2024} />
+    <Footer container className="bg-gradient-to-r from-indigo-900 to-indigo-700 px-10 text-slate-100 ">
+      <Footer.Copyright href="/home" by="PSU Calendar™" year={2024} className="text-white"/>
       <Footer.LinkGroup>
-        <Footer.Link href="/home">Calendar</Footer.Link>
-        <Footer.Link href="/Google">Google</Footer.Link>
+        <Footer.Link href="/home" className="text-white">Calendar</Footer.Link>
+        <Footer.Link href="/Google" className="text-white">Google</Footer.Link>
       </Footer.LinkGroup>
     </Footer>
   );
